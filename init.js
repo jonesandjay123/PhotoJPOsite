052b0136626e3122b4e644fc3b9f1f7abf472a78
Jones <jonesandjay123@gmail.com>
2018-09-25 09:58:45 +0800

    init.js switch to PhotoJPO

--- init.js
+++ init.js
@@ -1,9 +1,9 @@
 if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
 firebase.initializeApp({
-    apiKey: "AIzaSyCl_SZpy_jPglg0T0f4a6uxafyNDYLU-q4",
-	authDomain: "test-2292b.firebaseapp.com",
-    databaseURL: "https://test-2292b.firebaseio.com",
-    projectId: "test-2292b",
-    storageBucket: "test-2292b.appspot.com",
-    messagingSenderId: "289972332728"
+    apiKey: "AIzaSyBJonoqm9dtcFVh7JAd_ajLahkELF_rXws",
+	authDomain: "photojpo-74487.firebaseapp.com",
+    databaseURL: "https://photojpo-74487.firebaseio.com",
+    projectId: "photojpo-74487",
+    storageBucket: "photojpo-74487.appspot.com",
+    messagingSenderId: "185975663716"
 });
